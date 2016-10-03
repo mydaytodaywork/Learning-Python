f8bd4d4f411f1742cd57c5e16823a8c80d38d9b5
mydaytodaywork <kamal.nitrkl@gmail.com>
2016-10-03 18:05:27 +0530

    Add files via upload

---
+++ multiple_inputs_in_a_line.py
@@ -0,0 +1,7 @@
+#test=[int(x) for x in input().split()]
+#print(test)
+#test.sort()
+#print(test)
+import sys
+test=map(int,sys.stdin.readline().split())
+print(test)
---
+++ quine.py
@@ -0,0 +1,3 @@
+#a program that prints itself is known as quine
+a=10
+print(open(__file__).read())
